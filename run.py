73dbcc8dc4d4ad796eaae5810ef4bf03bcdbfd52
eddy-bryan <>
2023-11-20 19:25:29 +0000

    Added step to validate user input when asking for an action

--- run.py
+++ run.py
@@ -18,15 +18,30 @@ def display_menu():
     Displays a menu of actions the user can select from
     """
     print('Welcome to Terminal To Do!')
-    
     print('_' * 80 + '\n')
-
     print('Main menu:\n\n- Show Tasks\n- New Task\n- Complete Task\n- Exit')
-
     print('_' * 80 + '\n')
 
-    action = input("What would you like to do:\n")
-    print(f"You selected '{action}'.")
+    while True:
+        action = input("What would you like to do:\n").capitalize()
+
+        if action == 'Show tasks':
+            print(f"You selected '{action}'.")
+            # show_tasks()
+        elif action == 'New task':
+            print(f"You selected '{action}'.")
+            # new_tasks()
+        elif action == 'Complete task':
+            print(f"You selected '{action}'.")
+            # complete_task()
+        elif action == 'Exit':
+            print("Exiting program...")
+            break
+        else:
+            print('Action invalid, please select an option from the "Main menu".')
+            print('_' * 80 + '\n')
+            print('Main menu:\n\n- Show Tasks\n- New Task\n- Complete Task\n- Exit')
+            print('_' * 80 + '\n')
 
 
 display_menu()
